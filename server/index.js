e0627fd916b26f533b1d9ef96493bc1a6ca4f61b
dmitriy.turovtsov <web.best.master@gmail.com>
2016-06-24 16:47:47 +0300

    FileHunter - refactoring

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
+var FileHunter = require('./file-hunter');
 
-var fileHunter = require('./file-hunter');
+var fileHunter = new FileHunter();
 
 fileHunter.root = path.normalize([__dirname, '..', 'files'].join(path.sep));
 
@@ -13,5 +14,3 @@ var server = new http.createServer(function (req, res) {
 });
 
 server.listen(3000);
-
-
